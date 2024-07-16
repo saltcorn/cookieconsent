deb3b7e309cc33c2d9283a5796deda7376d29ff1
Tom Nielsen <tanielsen@gmail.com>
2024-07-16 20:07:30 +0100

    language, css

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const headers = () => [
       require("./package.json").version
     }/cookieconsent-config.js`,
   },
+  {
+    css: `/plugins/public/cookieconsent@${
+      require("./package.json").version
+    }/cookieconsent.css`,
+  },
 ];
 
 const buildConfigJS = async (context) => {
@@ -51,7 +56,64 @@ const buildConfigJS = async (context) => {
       flipButtons: context.pref_flip_btns,
     },
   };
-  const opts = { categories, guiOptions };
+  const language = {
+    default: "en",
+    autoDetect: "browser",
+    translations: {
+      en: {
+        consentModal: {
+          title: "Hello traveller, it's cookie time!",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+          acceptAllBtn: "Accept all",
+          acceptNecessaryBtn: "Reject all",
+          showPreferencesBtn: "Manage preferences",
+          footer:
+            '<a href="#link">Privacy Policy</a>\n<a href="#link">Terms and conditions</a>',
+        },
+        preferencesModal: {
+          title: "Consent Preferences Center",
+          acceptAllBtn: "Accept all",
+          acceptNecessaryBtn: "Reject all",
+          savePreferencesBtn: "Save preferences",
+          closeIconLabel: "Close modal",
+          serviceCounterLabel: "Service|Services",
+          sections: [
+            {
+              title: "Cookie Usage",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            },
+            {
+              title:
+                'Strictly Necessary Cookies <span class="pm__badge">Always Enabled</span>',
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              linkedCategory: "necessary",
+            },
+            {
+              title: "Analytics Cookies",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              linkedCategory: "analytics",
+            },
+            {
+              title: "Advertisement Cookies",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              linkedCategory: "marketing",
+            },
+            {
+              title: "More information",
+              description:
+                'For any query in relation to my policy on cookies and your choices, please <a class="cc__link" href="#yourdomain.com">contact me</a>.',
+            },
+          ],
+        },
+      },
+    },
+  };
+  const opts = { categories, guiOptions, language };
   const content = `CookieConsent.run(${JSON.stringify(opts)})`;
   await fs.writeFile(
     join(__dirname, "public", "cookieconsent-config.js"),
@@ -185,4 +247,7 @@ module.exports = {
   plugin_name: "cookieconsent",
   headers,
   configuration_workflow,
+  onLoad: async (configuration) => {
+    await buildConfigJS(configuration);
+  },
 };
